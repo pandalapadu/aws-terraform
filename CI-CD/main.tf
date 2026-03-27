b39e3e1a40705184641601994685638397e4150f
VenkatReddy <venkat@gmail.com>
2026-03-27 11:42:57 +0530

    AWS Terraform

--- CI-CD/main.tf
+++ CI-CD/main.tf
@@ -1,18 +1,66 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
 
-  owners = ["amazon"]
+# Get available AZs dynamically
+data "aws_availability_zones" "available" {}
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-resource "aws_instance" "demo" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t3.micro"
+# Create VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Terraform-GitHub-Actions"
+    Name = "CICD-main-vpc"
   }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Create Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Create Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# Create Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Create Route to Internet
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Associate Public Subnet with Route Table
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
 }
