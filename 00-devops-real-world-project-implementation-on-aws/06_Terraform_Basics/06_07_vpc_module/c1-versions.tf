5d4ae5a8216a9c25a19d6cf1369df7cc1ee18a96
VenkatReddy <venkat@gmail.com>
2026-04-03 13:38:50 +0530

    AWS Terraform

---
+++ 00-devops-real-world-project-implementation-on-aws/06_Terraform_Basics/06_07_vpc_module/c1-versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+  }
+  # Remote Backend #it should be already created before executing
+  # we can execute 06_05_remote_backend should execute it
+  backend "s3" {
+    bucket         = "tfstate-dev-ap-south-1-51clgm"
+    key            = "vpc/dev/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
